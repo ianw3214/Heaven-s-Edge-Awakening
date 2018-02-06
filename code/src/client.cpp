df52f21093d5aa80f0053d4b45fa33487e964c25
Ian <ianw3214@gmail.com>
2018-02-06 16:13:35 -0500

    Add server response to client

--- code/src/client.cpp
+++ code/src/client.cpp
@@ -11,13 +11,19 @@ int main(int argc, char* argv[]) {
         std::cout << "Failed to bind socket port\n";
         return 1;
     }
-    socket.set_nonblocking();
 
     // 54.208.98.2
     Address server(54, 208, 98, 2, 9999);
 
     char packet_data[] = "test message\n";
     socket.send(server, packet_data, sizeof(packet_data));
+
+    // receive a response from the server
+    char response[256];
+    Address temp;
+    socket.receive(temp, response, sizeof(response));
+
+    std::cout << "SERVER RESPONSE: " << response << '\n';
 
     socket_cleanup();
 
--- code/src/server.cpp
+++ code/src/server.cpp
@@ -17,12 +17,16 @@ int main(int argc, char* argv[]) {
         unsigned char packet_data[256];
 
         socket.receive(sender, packet_data, sizeof(packet_data));
-        
+
         std::cout << "CLIENT ADDRESS: " << sender.getAddress() << '\n';
         std::cout << "CLIENT PORT: " << sender.getPort() << '\n';
 
         // just print the packet for now
         std::cout << packet_data << '\n';
+
+        // send a test data packet back
+        char response[] = "Server response";
+        socket.send(sender, response, sizeof(response));
     }
 
     socket_cleanup();
