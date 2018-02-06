2514ae3811685fba98132106c9efc1737ae2db97
Ian <ianw3214@gmail.com>
2018-02-06 14:54:48 -0500

    Make server blocking so loop doesn't pre emptively exit

--- code/src/server.cpp
+++ code/src/server.cpp
@@ -11,15 +11,12 @@ int main(int argc, char* argv[]) {
         std::cout << "Failed to bind socket port\n";
         return 1;
     }
-    socket.set_nonblocking();
 
     while (true) {
         Address sender;
         unsigned char packet_data[256];
 
-        if (socket.receive(sender, packet_data, sizeof(packet_data)) <= 0){
-            break;
-        }
+        socket.receive(sender, packet_data, sizeof(packet_data)) <= 0)
         std::cout << "CLIENT ADDRESS: " << sender.getAddress() << '\n';
         std::cout << "CLIENT PORT: " << sender.getPort() << '\n';
 
