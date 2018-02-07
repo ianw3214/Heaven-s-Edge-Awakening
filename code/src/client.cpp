b5b267d4e95a4adefc2671f78e24ffc69698b32b
Ian <ianw3214@gmail.com>
2018-02-07 11:32:00 -0500

    Implement communication between server and client

--- code/src/client.cpp
+++ code/src/client.cpp
@@ -1,6 +1,24 @@
 #include "lib.h"
 
 #include <iostream>
+#include <string>
+
+#if PLATFORM == PLATFORM_WINDOWS
+#include "mingw.thread.h"
+#else
+#include <thread>
+#endif
+
+void receive(Socket socket) {
+    while (true) {
+        // receive a response from the server
+        char response[256];
+        Address temp;
+        socket.receive(temp, response, sizeof(response));
+
+        std::cout << response << '\n';
+    }
+}
 
 int main(int argc, char* argv[]) {
 
@@ -15,15 +33,21 @@ int main(int argc, char* argv[]) {
     // 54.208.98.2
     Address server(54, 208, 98, 2, 9999);
 
-    char packet_data[] = "test message\n";
+    char packet_data[] = "init message\n";
     socket.send(server, packet_data, sizeof(packet_data));
 
-    // receive a response from the server
-    char response[256];
-    Address temp;
-    socket.receive(temp, response, sizeof(response));
+    std::thread receiveThread(receive, socket);
+    receiveThread.detach();
 
-    std::cout << "SERVER RESPONSE: " << response << '\n';
+    while(true) {
+        std::string input;
+        std::getline(std::cin, input);
+        if (input == "quit") {
+            break;
+        } else {
+            socket.send(server, input.c_str(), input.size() + 1);
+        }
+    }
 
     socket_cleanup();
 
--- code/src/server.cpp
+++ code/src/server.cpp
@@ -1,15 +1,49 @@
 #include "lib.h"
 
 #include <iostream>
+#include <string>
 
-int main(int argc, char* argv[]) {
+#if PLATFORM == PLATFORM_WINDOWS
+#include "mingw.thread.h"
+#else
+#include <thread>
+#endif
 
-    socket_init();
+#define MAX_CLIENTS 10
+
+// global variables to keep track of clients
+Address clients[MAX_CLIENTS];
+
+// helper function to add clients to the client list
+bool add_client(Address client) {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i].getAddress() == 0) {
+            clients[i] = client;
+            return true;
+        }
+    }
+    return false;
+}
+
+// send the message from the sender to all other clients
+void send_message(Socket socket, char message[], Address sender) {
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i].getAddress() == sender.getAddress() && clients[i].getPort() == sender.getPort()) {
+            break;
+        }
+        if (clients[i].getAddress() != 0) {
+            socket.send(sender, message, 256);
+        }
+    }
+}
+
+// thread to receive incoming packets
+void receive() {
 
     Socket socket;
     if (!socket.open(9999)) {
         std::cout << "Failed to bind socket port\n";
-        return 1;
+        return;
     }
 
     while (true) {
@@ -17,16 +51,34 @@ int main(int argc, char* argv[]) {
         unsigned char packet_data[256];
 
         socket.receive(sender, packet_data, sizeof(packet_data));
+        // print out the message from the client
+        std::cout << packet_data << "\n---\n";
 
-        std::cout << "CLIENT ADDRESS: " << sender.getAddress() << '\n';
-        std::cout << "CLIENT PORT: " << sender.getPort() << '\n';
+        // if the client is not already stored, add the client to the list
+        for (int i = 0; i < MAX_CLIENTS; ++i) {
+            if (clients[i].getAddress() != sender.getAddress() || clients[i].getPort() != sender.getPort()) {
+                add_client(sender);
+            }
+        }
 
-        // just print the packet for now
-        std::cout << packet_data << '\n';
+        // then send the received message to the clients
+        send_message(socket, (char*) packet_data, sender);
+    }
+}
 
-        // send a test data packet back
-        char response[] = "Server response";
-        socket.send(sender, response, sizeof(response));
+int main(int argc, char* argv[]) {
+
+    socket_init();
+
+    std::thread receiveThread(receive);
+    receiveThread.detach();
+
+    while(true) {
+        std::string input;
+        std::getline(std::cin, input);
+        if (input == "quit") {
+            break;
+        }
     }
 
     socket_cleanup();
